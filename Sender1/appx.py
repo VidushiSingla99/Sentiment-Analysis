9425f1714f7e5e1d98ba99dc0c7377ec22992bcc
VidushiSingla99 <87124790+VidushiSingla99@users.noreply.github.com>
2021-10-24 19:46:14 +0530

    Update appx.py

--- Sender1/appx.py
+++ Sender1/appx.py
@@ -33,39 +33,30 @@ def my_form_0():
 
     return render_template('form.html', final=compound, text1=textt,text2=score['pos'],text5=score['neg'],text4=compound,text3=score['neu'])
 
-class RabbitMq(object):
-     def __init__(self):
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='localhost'))
+      
 
-        self.channel = self.connection.channel()
+connection=pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel=connection.channel()
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        self.callback_queue = result.method.queue
+channel.queue_declare(queue='worker_queue'.durable=True)
 
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True)
+channel.basic_publish(
+	exchange='',
+	routing_key='worker_queue',
+	body=message,
+	properties=pika.BasicProperties(
+		delivery_mode=2,
+	))
 
-     def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
+print("[x] Sent %r" % message)
+connection.close()
+        
 
-     def call(self, n):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='task_queue',
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body=str(n))
-        while self.response is None:
-            self.connection.process_data_events()
-        return (self.response)
+if __name__ == "__main__":
+    app.run()
+    
+
+    
        
 
 sentimentAnalysis=RabbitMq()
